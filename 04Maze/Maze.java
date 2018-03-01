dda1d9d703bdd635a66aeb5506af42e305a7664f
ichasen <ichasen@stuy.edu>
2018-03-01 15:05:53 -0500

    constructor still isn't right but solve method done

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -1,73 +1,85 @@
 import java.util.*;
 import java.io.*;
 public class Maze{
-
-
     private char[][]maze;
     private boolean animate;//false by default
-
-    
     public Maze(String filename){
 	try{
-        File x = new File (filename);
-	Scanner s = new Scanner (x);
-	int y = s.nextLine().length();
-	for (int i = 0;i<y;i++){
-	    for (int j = 0;j<y;j++){
-		while (s.hasNext()){
-		    maze[i][j] = s.nextLine().charAt(i);
+	    File x = new File (filename);
+	    Scanner s = new Scanner (x);
+	    int y = s.nextLine().length();
+	    for (int i = 0;i<y;i++){
+		for (int j = 0;j<y;j++){
+		    while (s.hasNext()){
+			maze[i][j] = s.nextLine().charAt(i);
+		    }
 		}
 	    }
-	}
 	}
 	catch (FileNotFoundException f){
 	    System.out.println("File not found");
 	}
     }
-    
-
     private void wait(int millis){
-         try {
-             Thread.sleep(millis);
-         }
-         catch (InterruptedException e) {
-         }
-     }
-
-
+	try {
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e) {
+	}
+    }
     public void setAnimate(boolean b){
-
-        animate = b;
-
+	animate = b;
     }
-
-
     public void clearTerminal(){
-
-        //erase terminal, go to top left of screen.
-
-        System.out.println("\033[2J\033[1;1H");
-
+	System.out.println("\033[2J\033[1;1H");
     }
-
-
-
-    
     public int solve(){
-	return solve (0,0);
+	return solveH (0,0);
     }
-    private int solve(int row, int col){
+    private int solveH(int row, int col){
         if(animate){
-
-            clearTerminal();
+	    clearTerminal();
             System.out.println(this);
-
-            wait(20);
+	    wait(20);
         }
-        
+	int ans = 0;
+        for (int i = 0;i<maze.length;i++){
+	    for (int j = 0;j<maze[0].length;j++){
+		if (maze[i][j] == 'S'){
+		    maze[i][j] = '@';
+		}
+		if (maze[i][j] == 'E'){
+		    return ans;
+		}
+		if ((maze[i-1][j] == '@' || maze[i-1][j-1] == '@' || maze[i][j-1] == '@' || maze[i+1][j-1] == '@' || maze[i+1][j] == '@' || maze[i+1][j+1] == '@' || maze[i][j+1] == '@' || maze[i-1][j+1] == '@') && maze[i][j] == ' '){
+		    maze[i][j] = '@';
+		    return solveH(i,j);
+	        }
+		else{
+		    for (int x = 0;i<maze.length;i++){
+			for (int y = 0;j<maze[0].length;j++){
+			    if (maze[x][y] != '#'){
+				maze[x][y] = ' ';
+			    }
+			    if (maze[x][y] == 'S'){
+				return solveH (x,y);
+			    }
+			}
+		    }
+		}
+	    }
+	}
 
         return -1; 
     }
+    public static void main(String[]args){
+        Maze f;
+        f = new Maze("data1.dat");//true animates the maze.
+        
+        f.setAnimate(true);
+        f.solve();
 
+        System.out.println(f);
+    }
 
 }
