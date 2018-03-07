74b1e212e4960f1b080d3a69f9d97473d3c7f4de
Isaac <ichasen@stuy.edu>
2018-03-07 17:31:22 -0500

    edited countSolutions

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,6 +1,7 @@
 public class QueenBoard{
     private int[][] board;
     public QueenBoard(int size){
+	board = new int[size][size];
 	for (int i = 0;i<board.length;i++){
 	    for (int j = 0;j<board.length;j++){
 		board[i][j] = 0;
@@ -62,12 +63,56 @@ public class QueenBoard{
 	return false;
     }
     public int countSolutions(){
-	return countH (1,0);
+	return countH (0,0);
     }
-    public int countH(int ans,int current){
-	if (current >= board.length){
-	    return ans;
+    public int countH(int ans,int c){
+	if (c >= board.length){
+	    ans++;
 	}
-	return (ans + current) * ans;
+	else{
+	    for (int i = 0; i < board.length; i++){
+		for (int j = 0;j<board[0].length;j++){
+		    if (board[i][j] == 0){
+			addQueen(i,j);
+			countH(ans,c + 1);
+			removeQueen(i,j);
+		    }
+		}
+	    }
+	}
+	return ans;
+    }
+    public static void main(String[] args){
+    QueenBoard b = new QueenBoard(4);
+
+    System.out.println(b.solve()); //prints true
+    System.out.println(b); //prints a valid solution
+
+    try{
+      b.solve();
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+
+    try{
+      b.countSolutions();
+    }catch(IllegalStateException e){
+      System.out.println("Error: The board contains non-zero values");
+    } //prints "Error: The board contains non-zero values"
+
+    for (int i = 0; i < 12; i++){
+      QueenBoard a = new QueenBoard(i);
+      System.out.println("# of Solutions for " + i + ": " + a.countSolutions());
+      /*          Expected Values
+       i --> # of Solutions   i --> # of Solutions
+      0 --> 1                      6 --> 4
+      1 --> 1                      7 --> 40
+      2 --> 0                      8 --> 92
+      3 --> 0                      9 --> 352
+      4 --> 2                    10 --> 724
+      5 --> 10                  11 --> 2680
+      */
+      System.out.println(a); //prints out an empty i by i grid of underscores
+}
     }
 }
