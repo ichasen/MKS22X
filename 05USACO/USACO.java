ad7cab01960deae98fa403c7289fd2d464cdf9ec
Isaac <ichasen@stuy.edu>
2018-03-08 21:19:45 -0500

    added silver

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,18 +1,25 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.io.FileNotFoundException;
 import java.io.File;
 public class USACO{
     public static int bronze(String filename){
-	int height;
-	int rowStomp;
-	int colStomp;
-	int amountStomp;
-	File f = new File(filename);
-	Scanner s = new Scanner(f);
-	int r = Integer.parseInt(s.next());
-	int c = Integer.parseInt(s.next());
-	return bronzeH(rowStomp,colStomp,height,amountStomp);
+	try{
+	    int height;
+	    int rowStomp;
+	    int colStomp;
+	    int amountStomp;
+	    File f = new File(filename);
+	    Scanner s = new Scanner(f);
+	    int r = Integer.parseInt(s.next());
+	    int c = Integer.parseInt(s.next());
+	    return bronzeH(rowStomp,colStomp,height,amountStomp);
+	}
+	catch (FileNotFoundException e){
+	    System.exit(1);
+	}
+        return bronzeH(rowStomp,colStomp,height,amountStomp);
     }
     public static int bronzeH(int r,int c, int h, int a){
 	int[][] lake;
@@ -41,5 +48,22 @@ public class USACO{
 	    }
 	}
 	return depth * 72 * 72;
+    }
+    public static int silver(String filename){
+	int[][] oldNums;
+	int[][] newNums;
+	int start;
+	int end;
+	int numMoves;
+	char[][] map;
+	File f = new File(filename);
+	Scanner s = new Scanner(f);
+	for (int i = 0;i<map.length;i++){
+	    for (int j = 0;j<map[0].length;j++){
+		newNums[i][j] = oldNums[i][j];
+		return newNums[i][j];
+	    }
+	}
+	return 0;
     }
 }
