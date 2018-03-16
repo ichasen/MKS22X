44d93ae52a0ebf2e2a1b25cb7fed7da0a6824249
Isaac <ichasen@stuy.edu>
2018-03-16 18:12:28 -0400

    moved partition code to quicksort, redid quickselect

--- 06/Quick.java
+++ 06/Quick.java
@@ -1,70 +1,65 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Arrays;
+import java.lang.Math;
 public class Quick{
     public static int partition(int[] data, int start, int end){
 	Random r = new Random();
 	int pivot = data[r.nextInt(data.length)];
-	int x = data[start];
-	data[start] = data[pivot];
-	data[pivot] = x;
-	int small = start+1;
-	int i = small + 1;
-	int large = end;
-	while (i < large){
-	    if (data[i] > pivot){
-		int m = data[i];
-		int n = data[large];
-		data[i] = n;
-		data[large] = m;
-		large = large - 1;
-	    }
-	    else if (data[i] == pivot){
+	int i = start+1;
+	int small = start;
+	int large = end-1;
+	while(i <= large){
+	    if (data[i] == pivot){
 		i++;
 	    }
-	    else{
-		int a = data[small];
-		int b = data[i];
-		data[small] = b;
-		data[i] = a;
+	    else if (data[i] > pivot){
+		swap(data,i,large);
+		large--;
+	    }else{
+		swap(data,small,i);
 		small++;
 		i++;
 	    }
-`	}
-	if(data.length == 1){
-	    return large;
 	}
-	else{
-	    partition(data,start,small-1);
-	    partition(data,i,end);
-	}
-	return 0;
+	return large;
+    }
+
+    private static void swap(int[] data, int a, int b){
+	int x = data[a];
+	data[a] = data[b];
+	data[b] = x;
     }
     public static int quickselect(int[] data, int k){
-	Quick q = new Quick();
-	int x = q.partition(data,0,data.length);
-	int[] data2 = new int[q.partition(data,0,data.length-1)];
-	int[] data3 = new int[q.partition(data,k,data.length-1)];
-	if (x > k){
-	    for (int i = 0;i<data2.length;i++){
-		data2[i] = data[i];
+	int small = 0;
+	int large = data.length - 1;
+	while(small<large){
+	    int pivot = partition(data,small,large);
+	    if(pivot < k){
+		small = pivot + 1;
 	    }
-	    return quickselect(data2,k);
-	}
-	else if (x < k){
-	    for (int j = 0;j<data3.length;j++){
-		data3[j] = data[j];
+	    else if(pivot > k){
+		large = pivot - 1 ;
 	    }
-	    return quickselect(data3,0);
-	}
-	else{
-	    return data[q.partition(data,0,data.length-1)];
-	}
+	    else{
+		return data[pivot];
+	    }
+	}	    
+	return data[small];
     }
-    public static void quickSort(int[] data){
-	Quick q = new Quick();
-	if((q.partition(data,0,data.length)) != 1){
-	    partition(data,start,small-1);
-	    partition(data,i,end);
+
+    public static void quicksort(int[] data){
+	quickH(data,0,data.length - 1);
+    }
+
+    public static void quickH(int[] data,int start,int end){
+	if(data.length == 0){
+	    return;
+	}
+	if(start < end){
+	    int pivot = partition(data,start,end);
+	    quickH(data,start,pivot-1);	
+	    quickH(data,pivot+1,end);
 	}
     }
 }
