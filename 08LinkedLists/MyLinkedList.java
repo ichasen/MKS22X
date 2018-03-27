8ac207ece06b1bd1b5728d5f5a2c9633b767be34
Isaac <ichasen@stuy.edu>
2018-03-27 17:32:38 -0400

    started on phase 1

---
+++ 08LinkedLists/MyLinkedList.java
@@ -0,0 +1,31 @@
+public class MyLinkedList{
+    Node start;
+    Node end;
+    int size;
+    public MyLinkedList(){
+	size = 0;
+    }
+    public int size(){
+	return size;
+    }
+    public int get(int index){
+    }
+    public int set(int index,int newValue){
+    }
+    private class Node{
+	Node next;
+	Node prev;
+	int data;
+	public Node getNext(){
+	    return next;
+	}
+	public Node getPrev(){
+	    return prev;
+	}
+	public int getValue(){
+	}
+	public String toString(){
+	}
+    }
+}
+	
