e6b3fca73a8902a9371913daa5ae93f92c87eac0
ichasen <ichasen@stuy.edu>
2018-03-29 15:14:51 -0400

    Added Node methods, started on get, set, and add

--- 08LinkedLists/MyLinkedList.java
+++ 08LinkedLists/MyLinkedList.java
@@ -5,24 +5,33 @@ public class MyLinkedList{
     public MyLinkedList(){
 	size = 0;
     }
+    private Node getNode(int index){
+	
+    }
     public int size(){
 	return size;
     }
     public int get(int index){
-	
+	return getNode(index).getValue();
     }
     public int set(int index,int newValue){
-	return index;
+	getNode(index).setData(newValue);
     }
     public String toString(){
 	String ans = "[";
 	for (int i = 0;i<size;i++){
+	    ans += get(i);
+	    ans += ",";
 	}
 	ans += "]";
 	return ans;
     }
     public boolean add(int value){
 	Node n = new Node();
+	n.setData(value);
+	getNode(end-1).setNext(value);
+	for (int i = 0;i<end-1;i++){
+	    getNode(i).setNext
 	size += 1;
 	return true;
     }
@@ -39,8 +48,19 @@ public class MyLinkedList{
 	public int getValue(){
 	    return data;
 	}
+	public void setNext(Node newNext){
+	    next = newNext;
+	}
+	public void setPrev(Node newPrev){
+	    prev = newPrev;
+	}
+	public void setData(Node newData){
+	    data = newData;
+	}
 	public String toString(){
-	    
+	    String ans = "";
+	    ans += data;
+	    return ans;
 	}
     }
 }
