cf202cb6cdb2668aa54e8c3aba43624d77840ebc
ichasen <ichasen@stuy.edu>
2018-04-13 15:34:39 -0400

    added SupppressWarnings

--- 08LinkedLists/Sorts.java
+++ 08LinkedLists/Sorts.java
@@ -1,3 +1,6 @@
+import java.util.*;
 public class Sorts{
     public static void radixsort(MyLinkedListImproved<Integer> data){
-	
+	@SuppressWarnings("unchecked") LinkedList<Integer>[] d = new LinkedList[10];
+    }
+}
