3d9edf5f37ee18d560356d71a9834b44059e8cb9
Isaac <ichasen@stuy.edu>
2018-04-29 14:30:08 -0400

    added add,remove,peek,size

--- 12/MyHeap.java
+++ 12/MyHeap.java
@@ -1,12 +1,14 @@
 public class MyHeap<String extends Comparable<String>>{
     String[] heap;
+    boolean isMax;
     @SuppressWarnings("unchecked")
     public MyHeap(){
-	heap = (String[]) new Object[0];
+	heap = (String[]) new Comparable[0];
     }
     @SuppressWarnings("unchecked")
     public MyHeap(boolean isMaxHeap){
 	heap = (String[]) new Object[0];
+	isMax = isMaxHeap;
     }
     public int indexOf(String element){
 	for (int i = 0;i<heap.length;i++){
@@ -17,7 +19,7 @@ public class MyHeap<String extends Comparable<String>>{
 	return -1;
     }
     private void pushDown(String parent,String child1,String child2,boolean isMaxHeap){
-	if (isMaxHeap){
+	if (isMax){
 	    if (child1.compareTo(child2) >= 0){
 	        String temp = parent;
 		heap[indexOf(parent)] = child1;
@@ -41,5 +43,36 @@ public class MyHeap<String extends Comparable<String>>{
 		heap[indexOf(child2)] = temp;
 	    }
 	}
+    }
+    public String peek(){
+	return heap[0];
+    }
+    public int size(){
+	return heap.length;
+    }
+    public void add(String element){
+	@SuppressWarnings("unchecked") String[] heap2 = (String[]) new Comparable[heap.length + 1];
+	for (int i = 0;i<heap.length;i++){
+	    heap2[i] = heap[i];
+	}
+	heap2[heap2.length - 1] = element;
+	if (isMax){
+	    if (heap2[(heap2.length - 2)/2].compareTo(element) < 0){
+		pushDown(heap2[(heap2.length - 2)/2],element,heap2[heap2.length - 2],true);
+	    }
+	}
+	else{
+	    if (heap2[(heap2.length - 2)/2].compareTo(element) > 0){
+		pushDown(heap2[(heap2.length - 2)/2],element,heap2[heap2.length - 2],false);
+	    }
+	}
+    }
+    public String remove(){
+	String ans = heap[heap.length - 1];
+	@SuppressWarnings("unchecked") String[] heap2 = (String[]) new Comparable[heap.length - 1];
+	for (int i = 0;i<heap2.length;i++){
+	    heap2[i] = heap[i];
+	}
+	return ans;
     }
 }
