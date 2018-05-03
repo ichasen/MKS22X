716b9bf0eac6a2e0a2f878d349dddb6c8359b1c2
Isaac <ichasen@stuy.edu>
2018-05-03 18:14:15 -0400

    edited add

--- 13Medians/RunningMedians.java
+++ 13Medians/RunningMedians.java
@@ -1,9 +1,24 @@
 public class RunningMedians{
-    MyHeap maxHeap = new MyHeap();
-    MyHeap minHeap = new MyHeap();
-    public void add(Integer element){
-	if (maxHeap.length == 0 && minHeap.length == 0){
+    MyHeap maxHeap = new MyHeap<Double>();
+    MyHeap minHeap = new MyHeap<Double>();
+    public void add(Double element){
+	if (maxHeap.size() == 0 && minHeap.size() == 0){
 	    maxHeap.add(element);
 	}
+	else if (element.compareTo(maxHeap.peek()) > 0){
+	    minHeap.add(element);
+	    if (minHeap.size() - maxHeap.size() > 1){
+		resize(maxHeap,minHeap);
+	    }
+	}
+	else{
+	    minHeap.add(element);
+	    if (maxHeap.size() - minHeap.size() > 1){
+		resize(minHeap,maxHeap);
+	    }
+	}
+    }
+    public void resize(MyHeap<Double> heap1, MyHeap<Double> heap2){
+        heap1.add(heap2.remove());
     }
 }
