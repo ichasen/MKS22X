34a21acfc0a3c2a58606962c91f6cc4c244c64a3
Isaac <ichasen@stuy.edu>
2018-05-13 09:22:36 -0400

    having really weird runtime issues

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,37 +1,63 @@
 import java.util.Arrays;
 public class Merge{
+    public static void insertionSort(int[] data){
+	for (int i = 1; i < data.length; i++){
+	    int key = data[i];
+	    int j = i-1;
+	    while (j >= 0 && data[j] > key){
+		data[j+1] = data[j];
+		j = j-1;
+	    }
+	    data[j+1] = key;
+	}
+    }
     public static void mergesort(int[] data){
 	int[] temp = new int [data.length];
-	msort(data,temp,0,data.length - 1);
+	for (int i = 0;i<data.length;i++){
+	    temp[i] = data[i];
+	}
+	msort(data,temp,0,data.length-1);
     }
     public static void msort(int[] data, int[] temp, int lo, int hi){
-	if (lo >= hi){
+	if (lo>=hi){
 	    return;
 	}
-	int mid = (lo + hi)/2 + 1;
-	msort(temp,data,lo,mid);
-	msort(temp,data,mid+1,hi);
-	merge(data,temp,lo,mid,hi);
+	else if (hi-lo <= 10){
+	    insertionSort(data);
+	}
+	else{
+	    int mid = (lo + hi)/2;
+	    msort(temp,data,lo,mid);
+	    msort(temp,data,mid+1,hi);
+	    merge(data,temp,lo,mid,hi);
+	}
     }
     public static void merge(int[] data,int[] temp,int lo,int mid,int hi){
-	int small = lo;
-	int i = 0;
+	int tracker = lo;
+	int i = lo;
+	int x = mid+1;
 	int large = hi;
-	while (small<mid && mid<=large){
-	    if (data[small] <= data[mid]){
-		temp[i] = data[small];
-		small++;
+	while (i<=mid && x<=large){
+	    if (data[i] <= data[x]){
+		data[tracker] = temp[i];
 		i++;
 	    }
-	    else if (temp[small] > temp[mid]){
-		temp[i] = data[mid];
-		mid++;
-		i++;
+	    else{
+		data[tracker] = temp[x];
+		x++;
 	    }
+	    tracker++;
 	}
-	for(int j = small;j<=large;j++){
-	    data[j] = temp[j];
-	}	
+	while (i<=mid){
+	    data[tracker] = temp[i];
+	    i++;
+	    tracker++;
+	}
+	while (mid<=hi){
+	    data[tracker] = temp[mid];
+	    i++;
+	    x++;
+	}
     }
     private static final int INCREASE = 0;
     private static final int DECREASE = 1;
