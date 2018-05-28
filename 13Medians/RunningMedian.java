660acee4e0fb430eb48888765ed74d1548713298
Isaac <ichasen@stuy.edu>
2018-05-28 08:33:31 -0400

    made small changes, tested

--- 13Medians/RunningMedian.java
+++ 13Medians/RunningMedian.java
@@ -1,38 +1,36 @@
 import java.util.*;
 import java.io.*;
 public class RunningMedian{
-    MyHeap maxHeap = new MyHeap<Double>();
-    MyHeap minHeap = new MyHeap<Double>(false);
+    MyHeap<Double> maxHeap = new MyHeap<Double>();
+    MyHeap<Double> minHeap = new MyHeap<Double>(false);
     public void add(Double element){
 	if (maxHeap.size() == 0 && minHeap.size() == 0){
-	    maxHeap.add(element);
+	    minHeap.add(element);
 	}
 	else{
-	    if (element.compareTo(minHeap.peek()) < 0){
-		minHeap.add(element);
-		if (maxHeap.size() - minHeap.size() >= 2){
-		    maxHeap.add(minHeap.remove());
-		}
-	    }
-	    else{
+	    if (element > minHeap.peek()){
 		maxHeap.add(element);
 		if (maxHeap.size() - minHeap.size() >= 2){
 		    minHeap.add(maxHeap.remove());
 		}
 	    }
+	    else{
+		minHeap.add(element);
+		if (minHeap.size() - maxHeap.size() >= 2){
+		    maxHeap.add(minHeap.remove());
+		}
+	    }
 	}
     }
     public Double getMedian(){
-	Double ans = 0.0;
-	int current = 0;
 	if (minHeap.size() >= maxHeap.size()){
-	    ans += minHeap[0];
-	    current++;
+	    return minHeap.peek();
 	}
 	if (maxHeap.size() > minHeap.size()){
-	    ans += maxHeap[0];
-	    current++;
+	    return maxHeap.peek();
 	}
-	return ans/current;
+	else{
+	    return (minHeap.peek() - maxHeap.peek()) / 2;
+	}
     }
 }
